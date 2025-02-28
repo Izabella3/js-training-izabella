1cf48d6a63143fba0599775d060ef94ad6da716d
Izabella3 <bella_s@purlin.com>
2025-02-28 20:50:47 +0400

    extra file

--- UserInput.js
+++
@@ -1,9 +0,0 @@
-
-  // Function to get input synchronously
-function userInput(message) {
-    const readlineSync = require("readline-sync");
-    let input = readlineSync.question(message ?? "Please enter your input: "); // Synchronously ask for input
-    return input;
-  }
-  
-  module.exports = { userInput };
